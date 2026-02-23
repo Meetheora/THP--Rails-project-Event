3378914065413e40dd5f8cd6566f146f0b08ce0b
crousty24-bit <allen.kch24@gmail.com>
2026-02-23 17:10:16 +0100

    Add models, migrations, and fixtures for Event, User, and Attendance

---
+++ app/models/event.rb
@@ -0,0 +1,26 @@
+class Event < ApplicationRecord
+  belongs_to :admin, class_name: "User"
+  has_many :attendances
+  has_many :participants, through: :attendances, source: :user
+  validates :start_date, presence: true
+  validates :duration, presence: true, numericality: {greater_than: 0}
+  validates :title, presence: true, length: {in: 5..140}
+  validates :description, presence: true, length: {in: 20..1000}
+  validates :price, presence: true, numericality: {in: 1..1000}
+  validates :location, presence: true
+  
+  validate :start_date_cannot_be_in_the_past
+  validate :duration_multiple_of_five
+
+  private
+  def start_date_cannot_be_in_the_past
+    return if start_date.blank?
+    if start_date < Time.current
+      errors.add(:start_date, "ne peut pas être dans le passé")
+    end
+  end
+
+  def duration_multiple_of_five
+    errors.add(:duration, "must be multiple of 5") if duration && duration % 5 != 0
+  end
+end
