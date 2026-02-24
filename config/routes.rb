a0ca55308ce03489445d1f39a8a6b993539edcc7
Meetheora <xenotimedev@gmail.com>
2026-02-24 16:36:38 +0100

    Users and a few views updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "events#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  
-  resources :events
 
   devise_for :users
+
+  resources :events
+  resources :users, only: [:show]
 end
