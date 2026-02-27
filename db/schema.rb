8bd990f22a35bba0c1367cfb64c485d2cfed31dc
Meetheora <xenotimedev@gmail.com>
2026-02-27 11:49:20 +0100

    generate migration for admin panel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_26_115031) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_27_104655) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_26_115031) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
